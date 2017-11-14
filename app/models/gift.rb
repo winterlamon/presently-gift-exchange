d481b45319d9a0c29620055d8e568c626885a644
github username <github email address>
2017-11-14 14:45:10 -0500

    API updated and gifts index functional

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,4 +2,9 @@ class Gift < ApplicationRecord
   has_many :swaps
   has_many :users, through: :swaps
   has_many :events, through: :swaps
+
+  def self.categories
+    @etsy
+  end
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
-require_relative 'application'
+require_all 'application'
+require_all 'lib'
 
 # Initialize the Rails application.
 Rails.application.initialize!
