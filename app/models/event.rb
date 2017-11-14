b39dc662b624c5e5d9b1324c1ae85e9d4b64152a
github username <github email address>
2017-11-14 11:33:04 -0500

    Event basics added

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   has_many :users, through: :user_events
   has_many :swaps
   has_many :gifts, through: :swaps
+
+  def my_receiver # How to get receiver_id name?
+    self.swaps.receiver_id
+  end
+
 end
