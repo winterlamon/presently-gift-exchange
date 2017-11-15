2c9ba70d084e27e173fb22108fccb0693d0dd131
github username <github email address>
2017-11-15 14:58:16 -0500

    added category class

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,9 +2,25 @@ class Gift < ApplicationRecord
   has_many :swaps
   has_many :users, through: :swaps
   has_many :events, through: :swaps
+  belongs_to :category
 
-  def self.categories
-    @etsy
-  end
-  
+  # def self.categories
+  #   @etsy
+  # end
+
+# def self.categories
+#   categories =[]
+#   get_all_etsy_listings.each do |listing|
+#   tax_path = listing[:taxonomy_path]
+#      categories << tax_path[0]
+#   end
+#     categories
+#   end
+
+  # the gift item price in the api is currently a string. we need to make it a float for the search params
+  #
+  # def price
+  #   self[:price].to_f
+  # end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-<<<<<<< HEAD
-=======
-
->>>>>>> d7947f2c7a03a380bac903124808d27d6a429ba2
 
 # Initialize the Rails application.
 Rails.application.initialize!
