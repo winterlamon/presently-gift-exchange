e43f804605f227e27d8cb0e62549ded8a7d3b36a
github username <github email address>
2017-11-15 16:25:54 -0500

    fixed create event link on user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :require_user
 
   def get_all_etsy_listings #parses all the data from JSON into array of hashes
     url = 'https://openapi.etsy.com/v2/listings/active?api_key=z6u2v4p18o5m8va3gpv5132a'
