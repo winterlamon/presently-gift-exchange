075a17de164433e7494c187fd876bf489c0d0472
github username <github email address>
2017-11-15 16:02:55 -0500

    added basic content to home view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def logged_in?
+    !!session[:user_id]
+  end
+
   def require_user
     redirect_to '/login' unless current_user
   end
